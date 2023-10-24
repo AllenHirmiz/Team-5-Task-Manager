3f743a5dd6ad0eb30a4529126c878f60eafd9e9f
AllenHirmiz <allenaliraqi@gmail.com>
2023-10-24 19:49:55 +1100

    add Todo table and it's fields

---
+++ server/models/Todo.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const todoSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  datecreated: {
+    type: Date,
+    default: Date.now,
+  },
+  duedate: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+  },
+});
+
+const Todo = model("Todo", todoSchema);
+
+module.exports = Todo;
